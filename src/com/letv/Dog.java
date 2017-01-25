c383913e00f62b5d09ceab81d738e5550eb2a30a
lijinlin <1575285636@qq.com>
2017-01-25 16:11:14 +0800

    创建了Mouse类

---
+++ src/com/letv/Dog.java
@@ -0,0 +1,7 @@
+package com.letv;
+
+/**
+ * Created by lijinlin on 2017/1/25.
+ */
+public class Dog {
+}
---
+++ src/com/letv/Mouse.java
@@ -0,0 +1,8 @@
+package com.letv;
+
+/**
+ * Created by lijinlin on 2017/1/25.
+ */
+public class Mouse {
+
+}
