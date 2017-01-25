95068d0b741f1966ee395b3202f625c0691a4a7f
lijinlin <1575285636@qq.com>
2017-01-25 15:35:50 +0800

    Initial commit

---
+++ src/com/letv/Main.java
@@ -0,0 +1,15 @@
+package com.letv;
+
+public class Main {
+    /**
+     * @param args
+     *
+     * 2017-01-25
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
+
+
